a6045648c7b9f01cde8bd641357801f65a7b6445
Dawn-Of-Justice <salosoja@gmail.com>
2025-02-18 23:08:44 +0530

    Add gevent and monkey patching for improved concurrency support

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, request, jsonify, redirect
 from flask_socketio import SocketIO, emit
 from werkzeug.middleware.proxy_fix import ProxyFix
